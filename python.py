9b86384e61aa1537a2c8cec259d1f1d53d9e92f9
flarebite <shakazz@protonmail.com>
2021-10-04 18:28:55 +0200

    Autopopulate transactions with account names, amount of prize and memo

--- python.py
+++ python.py
@@ -8,7 +8,29 @@ hive = Hive()
 hive.wallet.unlock("<KEY>")
 account = Account("lmac", blockchain_instance=hive)
 
-account.transfer("<ACCOUNT1>", "<AMOUNT>", "HIVE", "<MEMO>"),
-# account.transfer("<ACCOUNT2>", "<AMOUNT>", "HIVE", "<MEMO>"),
-# account.transfer("<ACCOUNT3>", "<AMOUNT>", "HIVE", "<MEMO>"),
-# (...)
+
+#size of prize pool in HIVE
+pool = 200
+
+#round of LMAC
+round = 93
+
+#list of winners in ranked order
+panel = ['<ACCOUNT1>', '<ACCOUNT2>', ... ,'<ACCOUNT15>' ]
+
+#prize ranks
+rank = ["first", "second", "third", "fourth", "fifth", "Top10", "Top15"]
+
+for i in panel:
+    #transactions rank 1-5
+    if n <= 4:
+        account.transfer(panel[n], "{:0.2f}".format(prize[n]), "HIVE", "Congratulations, your entry won the %s place in the LMAC Round %s!" % (rank[n], round));
+        n +=1;
+    #transactions Top 10
+    elif n <= 9:
+        account.transfer(panel[n], "{:0.2f}".format(prize[5]), "HIVE", "Congratulations, your entry made it in the %s in the LMAC Round %s!" % (rank[5], round));
+        n +=1;
+    #transactions Top 15
+    else:
+        account.transfer(panel[n], "{:0.2f}".format(prize[6]), "HIVE", "Congratulations, your entry made it in the %s in the LMAC Round %s!" % (rank[6], round));
+        n +=1;
